80e4176b737fed1f9d190d3a8e1c2d8c4ee10bd4
Yoojinjo <eugenejo111@gmail.com>
2024-09-05 12:49:02 -0500

    fixed error handling for app.listen

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
+}).post("/", (req, res) => {
+	const { name } = req.body;
+	res.send(`Hi, ${name}`);
+});
+
+app.listen(port, (error) => {
+	if (error) console.log("Error, can't start server", error);
+	else console.log(`Server listening on http://localhost:${port}`);
+});
